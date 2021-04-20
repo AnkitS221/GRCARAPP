21505d5c3e47ff96511bc58d18c120f445505887
Ankit Singh <Ankit.Singh@groupsoftus.com>
2021-04-20 13:04:16 +0530

    LN & TR changes

--- src/app/pages/po-details-edit/po-details-edit.page.ts
+++ src/app/pages/po-details-edit/po-details-edit.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BtnCellRendererComponent } from './btn-cell-renderer/btn-cell-renderer.component';
 import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-po-details-edit',
@@ -16,14 +17,22 @@ export class PoDetailsEditPage implements OnInit {
     // btnCellRenderer: BtnCellRendererComponent,
   }
   rowData = [
-    { make: 'Toyota', model: 'Celica', price: '', bal: 200 },
-    { make: 'Ford', model: 'Mondeo', price: '', bal: 500 },
-    { make: 'Porsche', model: 'Boxter', price: '', bal: 800 }
+    { make: 'Toyota', model: 100, price: 0, bal: 200 },
+    { make: 'Ford', model: 200, price: 0, bal: 500 },
+    { make: 'Porsche', model: 300, price: 0, bal: 800 }
   ];
   defaultColDef;
-  constructor(private location: Location) { }
+  constructor(private location: Location, private router: Router) { }
 
   ngOnInit() {
+    const navigation = this.router.getCurrentNavigation();
+    if(navigation.extras.state.transd === 'TR') {
+      this.rowData = [
+        { make: 'Toyota', model: 100, price: 200, bal: 200 },
+        { make: 'Ford', model: 200, price: 500, bal: 500 },
+        { make: 'Porsche', model: 300, price: 800, bal: 800 }
+      ];
+    }
     this.defaultColDef = {
       width: 100,
     };
--- src/app/pages/purchase-orders/purchase-orders.page.ts
+++ src/app/pages/purchase-orders/purchase-orders.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { FilterDetailsComponent } from './filter-details/filter-details.component';
-import { Router } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-purchase-orders',
@@ -10,7 +10,24 @@ import { Router } from '@angular/router';
 })
 export class PurchaseOrdersPage implements OnInit {
 
-  arr = Array<any>(16)
+  arr = [{
+    purchase_id: 'PO123456778',
+    subtext: '7 articles',
+    purchase_type: 'LN'
+  }, {
+    purchase_id: 'PO123456779',
+    subtext: '7 articles',
+    purchase_type: 'TR'
+  }, {
+    purchase_id: 'PO123456780',
+    subtext: 'Handling Units - 2',
+    purchase_type: 'LN'
+  }, {
+    purchase_id: 'PO123456781',
+    subtext: 'Handling Units - 2',
+    purchase_type: 'TR'
+  }];
+
   constructor(private modalCtrl: ModalController, private router: Router) { }
 
   ngOnInit() {
@@ -28,8 +45,13 @@ export class PurchaseOrdersPage implements OnInit {
     return await modal.present();
   }
 
-  goToEdit() {
-    this.router.navigate(['po-details-edit']);
+  goToEdit(type: string) {
+    const navigationExtras: NavigationExtras = {
+      state: {
+        transd: type,
+      }
+    };
+    this.router.navigate(['po-details-edit'], navigationExtras);
   }
 
 }
